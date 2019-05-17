fbcd7d874f43301be079769d47487092f4d4933f
Jessica Moore <jessica.cmoore1@gmail.com>
2019-05-17 13:05:11 -0400

    Implemented create instance method and allowed for an id to be assigned to each new contact

--- contact.py
+++ contact.py
@@ -1,17 +1,26 @@
 class Contact:
 
+  contacts = []
+  next_id = 1
+
   def __init__(self, first, last, email, note):
     """This method should initialize the contact's attributes"""
     self.first_name = first
     self.last_name = last
     self.email = email
     self.note = note
+    self.id = Contact.next_id
+    Contact.next_id += 1
 
   @classmethod
-  def create(cls):
+  def create(cls, first, last, email, note):
     """This method should call the initializer,
     store the newly created contact, and then return it
     """
+    contact = Contact(first, last, email, note)
+    cls.contacts.append(contact)
+
+    return contact
 
   @classmethod
   def all(cls):
@@ -57,5 +66,10 @@ class Contact:
   # Feel free to add other methods here, if you need them.
 
 
-contact = Contact('Betty', 'Maker', 'bettymakes@bitmakerlabs.com', 'Loves Pokemon')
-print(contact)
+contact1 = Contact.create('Betty', 'Maker', 'bettymakes@bitmakerlabs.com', 'Loves Pokemon')
+contact2 = Contact.create('Bit', 'Bot', 'bitbot@bitmakerlabs.com', 'beep boop')
+
+print(len(Contact.contacts))
+
+print(contact1.id)
+print(contact2.id)
